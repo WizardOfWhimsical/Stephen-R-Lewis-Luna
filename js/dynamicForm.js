5f3e2ae70b627163642e558fcf56f6b3e8724a26
WizardOfWhimsical <st.rayis1085@gmail.com>
2025-09-03 21:56:03 -0400

    finished basic run on setup/use case for visual on index.html. works fine none the less. calling it a night

--- js/dynamicForm.js
+++ js/dynamicForm.js
@@ -1,3 +1,4 @@
+// This script dynamically creates a leave message form and appends it to the DOM
 const section = document.createElement("section");
 const heading = document.createElement("h2");
 const form = document.createElement("form");
@@ -7,9 +8,12 @@ const inputName = document.createElement("input");
 const labelEmail = document.createElement("label");
 const inputEmail = document.createElement("input");
 const textArea = document.createElement("textarea");
-const button = document.createElement("button");
+const submitButton = document.createElement("button");
+const closeButton = document.createElement("button");
 const breakLine = document.createElement("br");
-const footer = document.querySelector("footer");//attching it to footer
+//attching it to footer
+const body = document.body;
+const footer = document.querySelector("footer");
 //section attributes and appending to the DOM
 section.setAttribute("id","leaveMessage");//for navigation
 // Setting text content for the heading
@@ -17,8 +21,14 @@ heading.textContent = "Leave a Message";
 //form attributes
 form.setAttribute("name","leaveMessage");
 //button attributes and text content
-button.setAttribute("type","submit");
-button.textContent = "Submit";
+submitButton.setAttribute("type","submit");
+submitButton.setAttribute("id","submit");
+closeButton.setAttribute("type","button");
+closeButton.setAttribute("id","close");
+submitButton.textContent = "Submit";
+closeButton.textContent = "Close";
+closeButton.classList.add("grabbingCursor");
+submitButton.classList.add("grabbingCursor");
 // Setting attributes and text content for the name input field
 labelName.setAttribute("for","name");
 labelName.textContent = "Name: ";
@@ -37,25 +47,37 @@ inputEmail.setAttribute("name","usersEmail");
 textArea.setAttribute("name","usersMessage");
 textArea.setAttribute("required","");
 
-
-
-
-
-
-
-
 // Appending elements to the form and section
 //so stoked when i found out about chainding append method calls, makes it so much cleaner
-form.append(labelName, inputName, breakLine, labelEmail, inputEmail, textArea, breakLine, button);
+form.append(labelName, inputName, breakLine, labelEmail, inputEmail, textArea, breakLine, submitButton, breakLine, closeButton);
 section.append(heading, breakLine, form);
 //change classes to modal.css later
 form.classList.add("centering");
-section.classList.add("centering");
+section.classList.add("leaveMessageModal");
+section.classList.add("hidden");
 
 // Appending the section to the body before the script tag
-const body = document.body;
-
 body.insertBefore(section, footer);
 
+//making button to toggle modal visibility for leave message form
+const leaveMessageButton = document.createElement("h2");
+leaveMessageButton.setAttribute("id","leaveMessageButton");
+// leaveMessageButton.setAttribute("type","button");
+leaveMessageButton.textContent = "Leave a Message";
+leaveMessageButton.classList.add("centering");
+leaveMessageButton.classList.add("grabbingCursor");
+body.insertBefore(leaveMessageButton, footer);
+// Adding event listener to toggle visibility of the leave message section
+leaveMessageButton.addEventListener("click", () => {
+  section.classList.toggle("hidden");
+  leaveMessageButton.classList.toggle("hidden");
+  console.log("Leave Message Button Clicked");
+});
+
+closeButton.addEventListener("click", () => {
+  section.classList.toggle("hidden");
+  leaveMessageButton.classList.toggle("hidden");
+  console.log("Close Button Clicked");
+});
 console.log("*********");
 console.log(section);
