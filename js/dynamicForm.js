73d7a100fbdde4a158f99acdeb91f39ae54e29d4
WizardOfWhimsical <st.rayis1085@gmail.com>
2025-09-10 13:06:52 -0400

    i finished tightening everything up

--- js/dynamicForm.js
+++ js/dynamicForm.js
@@ -5,19 +5,27 @@
 const section = document.createElement("section");
 const heading = document.createElement("h2");
 const form = document.createElement("form");
-const fieldset = document.createElement("fieldset");
+
 const labelName = document.createElement("label");
 const inputName = document.createElement("input");
 const labelEmail = document.createElement("label");
 const inputEmail = document.createElement("input");
 const textArea = document.createElement("textarea");
+
 const submitButton = document.createElement("button");
 const closeButton = document.createElement("button");
-// const breakLine = document.createElement("br"); not using this element, but keeping it for future reference
+
+const nameDiv = document.createElement("div");
+const emailDiv = document.createElement("div");
+const containerDiv = document.createElement("div");
+const btnContainerDiv = document.createElement("div");
+
+const leaveMessageButton = document.createElement("h2");
+
 const script = document.querySelector("script[src='../js/footer.js']");
+const footer = document.querySelector("footer");
 
 const body = document.body;
-const footer = document.querySelector("footer");
 
 section.setAttribute("id","leaveMessage");
 
@@ -25,52 +33,39 @@ heading.textContent = "Leave a Message";
 
 form.setAttribute("name","leaveMessage");
 
-submitButton.setAttribute("type","submit");
-submitButton.setAttribute("id","submit");
-closeButton.setAttribute("type","button");
-closeButton.setAttribute("id","close");
+closeButton.textContent = "Close";
+closeButton.classList.add("grabbingCursor","button-uniform","callToActionButton");
+setAttributes(closeButton, {"type":"button","id":"close" })
 
 submitButton.textContent = "Submit";
-closeButton.textContent = "Close";
+submitButton.classList.add("grabbingCursor","button-uniform","basicButton");
+setAttributes(submitButton,{"type":"submit","id":"submit"})
 
-closeButton.classList.add("grabbingCursor");
-closeButton.classList.add("button-uniform");
-closeButton.classList.add("callToActionButton");
-submitButton.classList.add("grabbingCursor");
-submitButton.classList.add("button-uniform");
-submitButton.classList.add("basicButton");
-
-labelName.setAttribute("for","name");
 labelName.textContent = "Name: ";
-inputName.setAttribute("type","text");
-inputName.setAttribute("id","name");
-inputName.setAttribute("required","");
-inputName.setAttribute("name","usersName");
+setAttributes(labelName, {
+  "for":"name", "type":"text", "id":"name",
+  "name":"usersName", "required":""
+});
 inputName.classList.add("spacingBelowInput");
 
-labelEmail.setAttribute("for","email");
 labelEmail.textContent = "Email: ";
-inputEmail.setAttribute("type","text");
-inputEmail.setAttribute("id","email");
-inputEmail.setAttribute("name","usersEmail");
-inputEmail.setAttribute("pattern","^([\\w]+)@([a-z]+)\\.([a-z]{3})$"
-);
-inputEmail.setAttribute("title","Please enter a valid email address");
-inputEmail.setAttribute("required",true);
+setAttributes(labelName, {
+"for":"email","type":"text","id":"email","name":"usersEmail","pattern":"^([\\w]+)@([a-z]+)\\.([a-z]{3})$","title":"Please enter a valid email address","required":true
+})
 inputEmail.classList.add("spacingBelowInput");
 
-textArea.setAttribute("name","usersMessage");
-textArea.setAttribute("placeholder","Leave a message here...");
-textArea.setAttribute("required","");
+setAttributes(textArea,{
+"name":"usersMessage", "placeholder":"Leave a message here...", "required":""
+})
 textArea.classList.add("spacingBelowInput");
 
-/*
-nameDiv, emailDiv, and containerDiv
-*/
-const nameDiv = document.createElement("div");
-const emailDiv = document.createElement("div");
-const containerDiv = document.createElement("div");
-const btnContainerDiv = document.createElement("div");
+form.classList.add("centering");
+section.classList.add("leaveMessageModal","hidden");
+
+leaveMessageButton.setAttribute("id","leaveMessageButton");
+leaveMessageButton.textContent = "Leave a Message";
+leaveMessageButton.classList.add("centering","personalizedButton","callToActionButton","grabbingCursor");
+
 nameDiv.append(labelName, inputName);
 emailDiv.append(labelEmail, inputEmail);
 containerDiv.append(nameDiv, emailDiv);
@@ -78,85 +73,9 @@ btnContainerDiv.append(submitButton, closeButton);
 form.append(containerDiv, textArea, btnContainerDiv);
 section.append(heading, form);
 
-// form.classList.add("centering");
-section.classList.add("leaveMessageModal");
-section.classList.add("hidden");
-
 body.insertBefore(section, footer);
-
-const leaveMessageButton = document.createElement("h2");
-leaveMessageButton.setAttribute("id","leaveMessageButton");
-leaveMessageButton.textContent = "Leave a Message";
-leaveMessageButton.classList.add("centering");
-leaveMessageButton.classList.add("personalizedButton");
-leaveMessageButton.classList.add("callToActionButton");
-leaveMessageButton.classList.add("grabbingCursor");
-
 body.insertBefore(leaveMessageButton, footer);
 
 leaveMessageButton.addEventListener("click", () => {
     navigateToMessagesPage();
 });
-
-/*************************
- * 
- *    My Locale Dev Model
- *
- * **************************/
-
-
-// function navigateToMessagesPage() {
-
-//   const currentPath = window.location.pathname;
-//   const fileName = currentPath.split("/").pop();
-
-//   switch (fileName) {
-//     case "index.html":
-//       window.location.href = "./html/messages.html";
-//       break;
-//     case "messages.html":
-//       // form.reportValidity();
-//         section.classList.toggle("hidden");
-//         leaveMessageButton.classList.toggle("visible");
-//         messagesSection.classList.toggle("visible");
-//       break;
-//     default:
-//       window.location.href = "./messages.html";
-//   }
-// }
-
-/*************************
- * 
- *  Github Production Model
- *
- * **************************/
-
-
-function navigateToMessagesPage() {
-  debugger;
-try {
-
-  const currentPath = window.location.pathname;
-  const fileName = currentPath.split("/").pop();
-
-  switch (fileName) {
-    // case "index.html":
-    //   window.location.href = "/Stephen-R-Lewis-Luna/html/messages.html";
-    //   break;
-    case "messages.html":
-        section.classList.toggle("hidden");
-        leaveMessageButton.classList.toggle("visible");
-        messagesSection.classList.toggle("visible");
-      break;
-    default:
-      window.location.href = "/Stephen-R-Lewis-Luna/html/messages.html";
-  }
-} catch (error) {
-  console.error("❌ JS Error:", error);
-}
-
-}
-
-
-
-
